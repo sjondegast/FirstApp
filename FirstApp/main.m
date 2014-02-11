f589cef06c5242c29864dbc6ee88ed7b9dda09ea
Sjon de Gast <sjondegast@gmail.com>
2014-02-11 12:14:52 +0100

    eerste test git, Xcode. line.. NSlog, sting aangepast

--- FirstApp/main.m
+++ FirstApp/main.m
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Programming is fun!");
         
     }
     return 0;
