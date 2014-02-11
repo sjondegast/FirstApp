32a11ff6840f7fa9fe3553187ad184fa9a92670d
Sjon de Gast <sjondegast@gmail.com>
2014-02-11 11:41:02 +0100

    Initial Commit

---
+++ FirstApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Sjon de Gast on 11-02-14.
+//  Copyright (c) 2014 Sjon de Gast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
